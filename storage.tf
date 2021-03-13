5044019201700d015895c1d4bee73e1c1c7ad6cf
Fabien Crespel <fabien@crespel.net>
2021-03-13 01:12:41 +0100

    Add GCS bucket for project files

--- storage.tf
+++ storage.tf
@@ -1,3 +1,12 @@
+// GCS bucket for project files
+resource "google_storage_bucket" "gcs_bucket_project" {
+  name                        = var.gcp_project_id
+  project                     = var.gcp_project_id
+  location                    = var.gcp_region
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
 // GCS bucket for backups
 resource "google_storage_bucket" "gcs_bucket_backups" {
   name                        = "${var.gcp_project_id}-backups"
