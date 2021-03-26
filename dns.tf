3293b2378fb8d75401dbb8e466d8051a3c14f2cf
Fabien Crespel <fabien@crespel.net>
2021-03-26 22:52:23 +0100

    Add GCP DNS zone

--- dns.tf
+++ dns.tf
@@ -7,3 +7,40 @@ resource "ovh_domain_zone_record" "dns_records" {
   ttl       = each.value.ttl
   target    = each.value.target
 }
+
+// GCP zone
+resource "google_dns_managed_zone" "dns_gcp_zone" {
+  name     = "gcp-${var.gcp_project_id}"
+  dns_name = "gcp.${var.dns_zone}."
+  project  = var.gcp_project_id
+  dnssec_config {
+    state = "on"
+  }
+}
+
+// GCP zone DNSSEC config
+data "google_dns_keys" "dns_gcp_keys" {
+  managed_zone = google_dns_managed_zone.dns_gcp_zone.id
+}
+
+// GCP zone NS record
+resource "ovh_domain_zone_record" "dns_gcp_ns_records" {
+  for_each  = toset(google_dns_managed_zone.dns_gcp_zone.name_servers)
+  zone      = var.dns_zone
+  subdomain = "gcp"
+  fieldtype = "NS"
+  ttl       = 0
+  target    = each.value
+}
+
+// GCP zone DS record
+/* DISABLED: not supported by OVH (yet)
+resource "ovh_domain_zone_record" "dns_gcp_ds_records" {
+  for_each  = { for ksk in data.google_dns_keys.dns_gcp_keys.key_signing_keys : ksk.id => ksk }
+  zone      = var.dns_zone
+  subdomain = "gcp"
+  fieldtype = "DS"
+  ttl       = 0
+  target    = each.value.ds_record
+}
+*/
