eae83d324a47cf63f21d1c5ac0b95d2e1c2fd16a
Fabien Crespel <fabien@crespel.net>
2021-12-12 20:35:02 +0100

    Infra: remove jobs chart

--- infra.tf
+++ infra.tf
@@ -102,15 +102,6 @@ resource "helm_release" "infra_elasticsearch" {
   values = [file("${path.module}/infra/values/elasticsearch.yaml")]
 }
 
-// Cron jobs
-resource "helm_release" "infra_jobs" {
-  name      = "jobs"
-  chart     = "${path.module}/infra/charts/jobs"
-  namespace = kubernetes_namespace.infra_ns.metadata[0].name
-
-  values = [file("${path.module}/infra/values/jobs.yaml")]
-}
-
 // Kibana
 resource "helm_release" "infra_kibana" {
   name      = "kibana"
